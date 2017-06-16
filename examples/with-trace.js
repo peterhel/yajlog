854f67e41b8e0dc45ce14422712348e9ad9acd9f
Peter Helenefors <peter@ridebite.com>
2017-06-16 09:56:05 +0200

    added withTrace function to print version and source module with log post

---
+++ examples/with-trace.js
@@ -0,0 +1,4 @@
+const yajlog = require('../dist');
+yajlog.withTrace();
+
+console.log('Where am I from?')
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-exports.configure = function (config) {
-    const log4js = require('log4js');
+const log4js = require('log4js');
+const path = require('path');
+const dirRx = new RegExp(__dirname);
+const nmRx = new RegExp('node_modules/');
+const rx = /(\(|)([^\s]+?)\d/;
 
+exports.configure = function (config) {
+    config = config || {};
     log4js.configure({
         replaceConsole: true,
         levels: {
@@ -10,5 +15,44 @@ exports.configure = function (config) {
             type: __dirname + '/appender.js',
             tokens: config.tokens
         }]
+    });
+}
+
+exports.withTrace = function (rootDir) {
+    let version;
+
+    rootDir || console.error(`'rootDir' was not given. Using ${__dirname}.`);
+
+    try {
+        version = require(path.join(rootDir || __dirname, 'package.json')).version;
+    } catch (error) {
+        console.error('Version could not be read from package.json.');
+        version = '0.0.0';
+    }
+
+    log4js.configure({
+        replaceConsole: true,
+        levels: {
+            '[all]': 'INFO'
+        },
+        appenders: [{
+            type: __dirname + '/appender.js',
+            tokens: {
+                version,
+                source: function () {
+                    // The caller:
+                    let stack = (new Error).stack.split("\n");
+                    stack.push(__dirname)
+
+                    let row;
+                    stack = stack.filter(_row => {
+                        return dirRx.test(_row) && !nmRx.test(_row);
+                    });
+                    const rxresult = rx.exec(stack[1]);
+                    const module = (rxresult && rxresult.length > 1) ? rxresult[2] : ''
+                    return module.replace(__dirname, '.');
+                }
+            }
+        }],
     });
 }
