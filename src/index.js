00688b6c0be67326e99c2f4b7d285b308efbb857
Peter Helenefors <peter@ridebite.com>
2017-06-16 15:18:33 +0200

    fixed source

--- src/index.js
+++ src/index.js
@@ -17,43 +17,26 @@ exports.configure = function (config) {
     });
 }
 
-exports.withTrace = function (rootDir) {
-    let version;
+exports.withTrace = function (rootDir, config) {
+    config = config || {};
 
-    rootDir || console.error(`'rootDir' was not given. Using ${__dirname}.`);
+    rootDir = rootDir || __dirname;
 
     const dirRx = new RegExp(rootDir);
 
-    try {
-        version = require(path.join(rootDir || __dirname, 'package.json')).version;
-    } catch (error) {
-        console.error('Version could not be read from package.json.');
-        version = '0.0.0';
+    config.tokens.source = function () {
+        // The caller:
+        let stack = (new Error).stack.split("\n");
+
+        let row;
+        stack = stack.filter(_row => {
+            return dirRx.test(_row) && !nmRx.test(_row);
+        });
+
+        const rxresult = rx.exec(stack[stack.length - 1]);
+        const module = (rxresult && rxresult.length > 1) ? rxresult[2] : ''
+        return module.replace(rootDir, '.');
     }
 
-    log4js.configure({
-        replaceConsole: true,
-        levels: {
-            '[all]': 'INFO'
-        },
-        appenders: [{
-            type: __dirname + '/appender.js',
-            tokens: {
-                version,
-                source: function () {
-                    // The caller:
-                    let stack = (new Error).stack.split("\n");
-                    stack.push(__dirname)
-
-                    let row;
-                    stack = stack.filter(_row => {
-                        return dirRx.test(_row) && !nmRx.test(_row);
-                    });
-                    const rxresult = rx.exec(stack[1]);
-                    const module = (rxresult && rxresult.length > 1) ? rxresult[2] : ''
-                    return module.replace(__dirname, '.');
-                }
-            }
-        }],
-    });
+    exports.configure(config);
 }
